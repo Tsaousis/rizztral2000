da52edd13f0364329664c58df8bc111f03c83cd0
Costopoulos <costopoulos.constantinos@gmail.com>
2025-02-01 02:28:36 +0200

    Add correct ingress

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ llm_contestant2 = ChatMistralAI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["https://huggingface.co/spaces/Mistral-AI-Game-Jam/Rizztral"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
